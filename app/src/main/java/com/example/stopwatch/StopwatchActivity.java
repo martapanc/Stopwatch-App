048a0dd1963b647d230d5d3da71c7dc67bbcc9e7
Marta Pancaldi <marta.panc@gmail.com>
2020-05-31 21:40:43 +0100

    Add working stopwatch (seconds only)

---
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -0,0 +1,55 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler timeHandler = new Handler();
+        timeHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.UK, "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                timeHandler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
